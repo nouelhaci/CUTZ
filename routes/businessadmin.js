f6d1c68793e5ee4ac85ace4faf89f07ce83879e1
nouelhaci <115217191+nouelhaci@users.noreply.github.com>
2023-07-02 17:21:04 -0400

    Update businessadmin.js

--- routes/businessadmin.js
+++ routes/businessadmin.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { SignUpBusinessAdmin, LoginBusinessAdmin,GetBusinessAdmin,UpdateBusinessAdmin,DeleteBusinessAdmin } = require('../controller/businessadmin');
+const { SignUpBusinessAdmin, LoginBusinessAdmin, GetBusinessAdmin, UpdateBusinessAdmin, DeleteBusinessAdmin, GetAllBusinessAdmin } = require('../controller/businessadmin');
 const { CheckUser } = require("../middleware/checkuser");
 
 router.route('/signup').post(SignUpBusinessAdmin)
 router.route('/login').post(LoginBusinessAdmin)
+router.route('/getall').get(GetAllBusinessAdmin)
 router.route('/').get(CheckUser, GetBusinessAdmin).delete(CheckUser, DeleteBusinessAdmin).patch(CheckUser, UpdateBusinessAdmin)
 
 
