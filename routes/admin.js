3c96be208ebedbed2f6593ec1c4ac8022c8209fb
nouelhaci <115217191+nouelhaci@users.noreply.github.com>
2023-07-02 17:20:51 -0400

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
-const { SignUpAdmin, LoginAdmin, GetAdmin, UpdateAdmin, DeleteAdmin } = require('../controller/admin');
+const { SignUpAdmin, LoginAdmin, GetAdmin, UpdateAdmin, DeleteAdmin, GetAllAdmins } = require('../controller/admin');
 const { CheckUser } = require("../middleware/checkuser");
 
 router.route('/signup').post(SignUpAdmin)
 router.route('/login').post(LoginAdmin)
-router.route('/').get(CheckUser, GetAdmin).delete(CheckUser, DeleteAdmin).patch(CheckUser, UpdateAdmin)
+router.route('/getall').get(GetAllAdmins)
+router.route('/:id').get(CheckUser, GetAdmin).delete(CheckUser, DeleteAdmin).patch(CheckUser, UpdateAdmin)
 
 
 
