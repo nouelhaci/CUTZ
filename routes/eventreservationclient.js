6239845c31e79b9c9c90dbdf104af8db229acb9b
nouelhaci <115217191+nouelhaci@users.noreply.github.com>
2023-07-02 17:22:46 -0400

    Update eventreservationclient.js

--- routes/eventreservationclient.js
+++ routes/eventreservationclient.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { CreateEventReservation,GetEventReservations,GetSingleEventReservation,UpdateEventReservation,DeleteEventReservation } = require('../controller/eventreservationclient')
+const { CreateEventReservation, GetEventReservations, GetSingleEventReservation, UpdateEventReservation, DeleteEventReservation, GetAllEventReservations } = require('../controller/eventreservationclient')
 const { CheckUser } = require('../middleware/checkuser')
 
-router.route('/').post(CheckUser,CreateEventReservation).get(CheckUser,GetEventReservations)
-router.route('/:id').get(CheckUser,GetSingleEventReservation).patch(CheckUser,UpdateEventReservation).delete(CheckUser,DeleteEventReservation)
+router.route('/').post(CheckUser, CreateEventReservation).get(CheckUser, GetEventReservations)
+router.route('/getall').get(GetAllEventReservations)
+router.route('/:id').get(CheckUser, GetSingleEventReservation).patch(CheckUser, UpdateEventReservation).delete(CheckUser, DeleteEventReservation)
 
 
 module.exports = router
