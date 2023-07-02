393184981ba4e4fb494660586f314a002ab01f66
nouelhaci <115217191+nouelhaci@users.noreply.github.com>
2023-07-02 17:24:02 -0400

    Update owner.js

--- routes/owner.js
+++ routes/owner.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { SignUpOwner, LoginOwner, GetOwner, UpdateOwner, DeleteOwner } = require('../controller/owner');
+const { SignUpOwner, LoginOwner, GetOwner, UpdateOwner, DeleteOwner, GetAllOwners } = require('../controller/owner');
 const { CheckUser } = require("../middleware/checkuser");
 
 router.route('/signup').post(SignUpOwner)
 router.route('/login').post(LoginOwner)
+router.route('/getall').get(GetAllOwners)
 router.route('/').get(CheckUser, GetOwner).delete(CheckUser, DeleteOwner).patch(CheckUser, UpdateOwner)
 
 
