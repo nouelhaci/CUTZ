acf54cbc55c33b94d620ec57d3435fd5efddf286
nouelhaci <115217191+nouelhaci@users.noreply.github.com>
2023-07-02 17:31:51 -0400

    Update client.js

--- routes/client.js
+++ routes/client.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
-const { SignUpClient, LoginClient, GetClient, DeleteClient, UpdateClient } = require('../controller/client')
+const { SignUpClient, LoginClient, GetClient, DeleteClient, UpdateClient, GetAllClients } = require('../controller/client')
 const { CheckUser } = require('../middleware/checkuser')
 
 router.route('/signup').post(SignUpClient)
 router.route('/login').post(LoginClient)
+router.route('/getall').get(GetAllClients)
 // router.route('/forgetpassword').patch(ForgetPassword)
-router.route('/').get(CheckUser, GetClient).delete(CheckUser, DeleteClient).patch(CheckUser, UpdateClient)
+router.route('/:id').get(CheckUser, GetClient).delete(CheckUser, DeleteClient).patch(CheckUser, UpdateClient)
 
 
 module.exports = router
