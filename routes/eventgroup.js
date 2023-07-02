e7bfc7cf5a8c078fd5b7b99046bed1742490c5ae
nouelhaci <115217191+nouelhaci@users.noreply.github.com>
2023-07-02 16:48:49 -0400

    Delete routes directory

--- routes/eventgroup.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { CreateEventGroup, GetEventGroups, GetSingleEventGroup, UpdateEventGroup, DeleteEventGroup } = require('../controller/eventgroup')
-
-router.route('/').post(CreateEventGroup).get(GetEventGroups)
-router.route('/:id').get(GetSingleEventGroup).patch(UpdateEventGroup).delete(DeleteEventGroup)
-
-
-module.exports = router
