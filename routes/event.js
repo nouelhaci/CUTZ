c973f6779741fbcfcf680daac83f58b960b96056
nouelhaci <115217191+nouelhaci@users.noreply.github.com>
2023-07-18 19:03:47 -0400

    Delete routes directory

--- routes/event.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { CreateEvent, GetEvents, GetSingleEvent, UpdateEvent, DeleteEvent } = require('../controller/event')
-
-router.route('/').post(CreateEvent).get(GetEvents)
-router.route('/:id').get(GetSingleEvent).patch(UpdateEvent).delete(DeleteEvent)
-
-
-module.exports = router
